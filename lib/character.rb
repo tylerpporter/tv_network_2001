f512e9fc4060cde026aeeebfaa32ddd867bb70d0
Tyler Porter <tylerpporter@gmail.com>
2020-02-11 09:11:28 -0700

    Finish iteration 1

--- lib/character.rb
+++ lib/character.rb
@@ -1,5 +1,19 @@
 class Character
+
   def initialize(character_info)
     @character_info = character_info
   end
-end 
+
+  def name
+    @character_info[:name]
+  end
+
+  def actor
+    @character_info[:actor]
+  end
+
+  def salary
+    @character_info[:salary]
+  end
+
+end
--- test/character_test.rb
+++ test/character_test.rb
@@ -8,7 +8,15 @@ class CharacterTest < Minitest::Test
   end
 
   def test_it_exists
+
     assert_instance_of Character, @kitt
+  end
+
+  def test_it_can_return_character_info
+
+    assert_equal "KITT", @kitt.name
+    assert_equal "William Daniels", @kitt.actor
+    assert_equal 1000000, @kitt.salary 
   end
 
 end
