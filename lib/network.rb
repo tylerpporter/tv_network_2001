847b41ccb342f84adc5c0119fe317c5d0dba3bf8
Tyler Porter <tylerpporter@gmail.com>
2020-02-11 09:44:23 -0700

    Create network class

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,8 @@
-
+class Network
+  attr_reader :name, :shows
+  
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+end
---
+++ test/network_test.rb
@@ -0,0 +1,22 @@
+require_relative 'test_helper.rb'
+require './lib/network.rb'
+require './lib/show.rb'
+require './lib/character.rb'
+
+class NetworkTest < Minitest::Test
+
+  def setup
+    @nbc = Network.new("NBC")
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+  end
+
+  def test_it_exists
+    assert_instance_of Network, @nbc
+  end
+
+end
