b32d6843106c01177d026f13f471c286df2d87c2
Tyler Porter <tylerpporter@gmail.com>
2020-02-11 09:21:23 -0700

    Create show class

---
+++ test/show_test.rb
@@ -0,0 +1,22 @@
+require_relative 'test_helper.rb'
+require './lib/character.rb'
+require './lib/show.rb'
+
+class ShowTest < Minitest::Test
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_it_exists_with_attributes
+
+    assert_instance_of Show, @knight_rider
+    assert_equal "Knight Rider", @knight_rider.name
+    assert_equal "Glen Larson", @knight_rider.creator
+    assert_equal Array, @knight_rider.characters.class
+    assert_equal Character, @knight_rider.characters[0].class
+  end  
+
+end
